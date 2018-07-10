c59d67f9601b74642fe4750376a878a791f0be39
Nana-Efua Essuman <nanaefua96@gmail.com>
2018-07-10 21:55:35 +0000

    Initialize FlashCard class

--- src/com/company/FlashCard.java
+++ src/com/company/FlashCard.java
@@ -1,4 +1,30 @@
 package com.company;
 
 public class FlashCard {
+    private String question;
+    private String answer;
+
+
+    public FlashCard(String q, String a) {
+        this.question = q;
+        this.answer = a;
+    }
+
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
 }
